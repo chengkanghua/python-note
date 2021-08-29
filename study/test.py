344cf289dd58c1c4933a3a58fc5af63621c9eadb
ckh <chengkanghua@foxmail.com>
2021-08-29 22:09:11 +0800

    模块2作业

--- study/test.py
+++ study/test.py
@@ -1,10 +1,16 @@
 # 基于列表推导式一行实现输出9*9乘法表。
 
 
-data = ['{}*{}={} '.format(i,j,i*j) for i in range(1,10) for j in range(1,10)]
+data = '\n'.join([" ".join(['{}*{}={}'.format(i,j,i*j) for i in range(1,j+1)]) for j in range(1,10)])
 print(data)
 
 data2 ="\n".join([" ".join(['{}*{}'.format(i, j) for j in range(1, i + 1)]) for i in range(1, 10)])
-print(data2)
+# print(data2)
 # [" ".join(['{}*{}'.format(i, j) for j in range(1, i + 1)]) for i in range(1, 10)]
 #          ['{}*{}'.format(i, j) for j in range(1, i + 1)]
+
+data3 = [" ".join(['{}*{}'.format(i, j) for j in range(1, i + 1)]) for i in range(1, 10)]
+print(data3)
+
+data4 = '\n'.join([" ".join(['{}*{}'.format(i, j) for j in range(1, i + 1)]) for i in range(1,10)])
+print(data4)
