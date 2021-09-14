762f2565f49ad88837b998924b20cc3f9c5790cc
ckh <chengkanghua@foxmail.com>
2021-09-15 02:06:11 +0800

    add"

--- study/test.py
+++ study/test.py
@@ -1,24 +1,19 @@
-# 请基于`__new__` 实现一个单例类（加锁）。
-import threading
+class C(object):
+
+    def getx(self):
+        print('getx')
+
+    def setx(self, value):
+        print('setx')
+
+    def delx(self):
+        print('delx')
+
+    x = property(getx, setx, delx, "I'm the 'x' property.")  #第四个位置是注释
 
 
-class Singleton(object):
-    instance = None
-    lock = threading.RLock()
+obj = C()
 
-    def __init__(self, name):
-        self.name = name
-
-    def __new__(cls, *args, **kwargs):
-        if cls.instance:
-            return cls.instance
-        with cls.lock:
-            if cls.instance:
-                return cls.instance
-            cls.instance = super().__new__(cls)
-        return cls.instance
-
-
-t1 = Singleton('aaa')
-t2 = Singleton('bbbb')
-print(t1 is t2)
+obj.x        #执行property里第一个位置的 函数 getx
+obj.x = 123  #执行property里第二个位置的 函数 setx
+del obj.x    #执行property里第三个位置的 函数 delx
