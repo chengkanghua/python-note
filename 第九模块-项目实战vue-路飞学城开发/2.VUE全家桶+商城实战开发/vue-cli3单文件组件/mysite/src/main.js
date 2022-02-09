f8b7ed44aaa2f393dda75ad8e0bc1f13b3ead98e
ckh <chengkanghua@foxmail.com>
2022-02-09 21:21:14 +0800

    vue mysite

--- 第九模块-项目实战vue-路飞学城开发/2.VUE全家桶+商城实战开发/vue-cli3单文件组件/mysite/src/main.js
+++ 第九模块-项目实战vue-路飞学城开发/2.VUE全家桶+商城实战开发/vue-cli3单文件组件/mysite/src/main.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
-import './plugins/element.js'
-Vue.config.productionTip = false;
 
+// 导入element-ui  
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)  // 全部倒入
+
+// 按需导入
+// import { Button,Table,TableColumn,InputNumber } from 'element-ui' 
+// Vue.use(Button)
+// Vue.use(Table)
+// Vue.use(TableColumn)
+// Vue.use(InputNumber)
+
+Vue.config.productionTip = false
 Vue.prototype.$http = axios;
 Vue.prototype.$bus = new Vue();
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
