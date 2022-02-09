df4908e5d52b431dace54c5b9994c2c95b7efdcd
ckh <chengkanghua@foxmail.com>
2022-02-09 13:09:47 +0800

    test

--- 第九模块-项目实战vue-路飞学城开发/2.VUE全家桶+商城实战开发/vue-cli3单文件组件/mysite/babel.config.js
+++ 第九模块-项目实战vue-路飞学城开发/2.VUE全家桶+商城实战开发/vue-cli3单文件组件/mysite/babel.config.js
@@ -1,5 +1,28 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  "presets": [
+    "@vue/cli-plugin-babel/preset"
+  ],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ],
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ],
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
