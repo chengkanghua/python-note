6ecc8f952ddeb1bc50de2a606c01f231be5fd96a
ckh <chengkanghua@foxmail.com>
2022-02-19 22:21:40 +0800

    cours

---
+++ 第九模块-项目实战vue-路飞学城开发/4-路飞学城/0-study/CorsDemo/middlewares.py
@@ -0,0 +1,10 @@
+from django.utils.deprecation import MiddlewareMixin
+
+class MyCors(MiddlewareMixin):
+    def process_response(self,request,response):
+        response["Access-Control-Allow-Origin"] = "*"
+        if request.method == "OPTIONS":
+            response["Access-Control-Allow-Methods"] = "PUT,DELETE"
+            response['Access-Control-Allow-Headers'] = "content-type"
+        return response
+
